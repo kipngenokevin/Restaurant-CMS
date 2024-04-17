a990fab2d1f91a2146c06de376decc6d25454bd5
kipngeno <kipngenokevin254@gmail.com>
2024-04-17 11:51:12 +0300

    successfully able to pass data from API into placeholders

--- web_dynamic/static/scripts/dashboard/edit-item.js
+++ web_dynamic/static/scripts/dashboard/edit-item.js
@@ -4,9 +4,32 @@ $(()=>{
         return urlParams.get(name);
     };
 
-    // Get the value of 'id' from the URL
-    var id = getUrlParameter('id');
+    let id = getUrlParameter('id');
+	console.log(id);
 
-    // Set the value of the HTML field
-    $('#field').val(id);
+	const fetchAPIData = () => {
+		$.ajax({
+			url: `https://kcodev.tech/restaurantcms/api/food_items/${id}`,
+			method: 'GET',
+			success: function(response) {
+				console.log("API Response:", response);
+				
+				$('#inputName').attr('placeholder', response.item_name);
+				$('#inputImage').attr('placeholder', response.item_image);
+                $('#inputDesc').attr('placeholder', response.item_description);
+                $('#inputFirstPrice').attr('placeholder', response.item_first_price);
+                $('#inputSecondPrice').attr('placeholder', response.item_second_price);
+                $('#inputIcon').attr('placeholder', response.icon_1); // Change to appropriate icon field
+                $('#inputCategory').attr('placeholder', response.item_category);
+			},
+			error: function(xhr, status, error){
+				setTimeout(() => {
+					fetchAPIData()
+				  }, 2000)
+			},
+		});
+	};
+
+	fetchAPIData();
+
 });
